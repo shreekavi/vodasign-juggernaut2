6b446ab42d3e2376d1d0bdb9e3bfb2ad42b1c0d1
Shree Dittakavi <shreekavi@gmail.com>
2010-09-11 22:40:43 +0530

    Trying out Juggernaut 2 and Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VodasignJuggernaut2::Application.config.secret_token = 'c182e621dda2a096f497d02a3d47c4e5d81d615bbd640f764c225b1ad26ce2866043d158f60a2769ee51ea4d236bf1309bc90fe0842347374992bd20daa69a3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+VodasignJuggernaut2::Application.config.session_store :cookie_store, :key => '_vodasign-juggernaut2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# VodasignJuggernaut2::Application.config.session_store :active_record_store
